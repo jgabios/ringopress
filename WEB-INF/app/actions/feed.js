dac86f4750fce4c40493ff68af4f714f278add35
jgabios <jajalinux@gmail.com>
2010-12-29 02:43:40 +0200

    when no posts exist, the last updated time in the feed is extracted from (new Date()) as opposed to now when an error raises when trying to get it from the last post.

--- WEB-INF/app/actions/feed.js
+++ WEB-INF/app/actions/feed.js
@@ -7,7 +7,7 @@ exports.action = require('action').action({
     "skin": "feed.xml",
     "getContext": function(env){
         var posts = bizpost(env).getFeedPosts();
-        var lastNewsTime = ringoDate.format(posts[0].createTime,'EEE, dd MMM yyyy HH:mm:ss Z');
+        var lastNewsTime = posts.length>0 ? ringoDate.format(posts[0].createTime,'EEE, dd MMM yyyy HH:mm:ss Z') : ringoDate.format(new Date(),'EEE, dd MMM yyyy HH:mm:ss Z');
         env.resp.contentType='text/xml';
         return {
             domain: env.req.host,
