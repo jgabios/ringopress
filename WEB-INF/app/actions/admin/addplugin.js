ea0a65d039db879a92ab8fa8cfaa85af86a6a5b1
jgabios <jajalinux@gmail.com>
2011-04-08 02:22:45 +0300

    start of the import plugin operation

--- WEB-INF/app/actions/admin/addplugin.js
+++ WEB-INF/app/actions/admin/addplugin.js
@@ -13,23 +13,28 @@ exports.action = require('action').action({
             return {};
         }
         if(env.req.isPost){
-            var plugin = new model.Plugin({'keyName': env.req.params['name']});
-            plugin['lastModified'] = new Date();
-            plugin['code'] = env.req.params['code'];
-            plugin['name'] = env.req.params['name'];
-            plugin['email'] = env.req.params['email'];
-            plugin['author'] = env.req.params['author'];
-            plugin['version'] = env.req.params['version'];
-            plugin['type'] = 'GDS';
-            plugin['activated'] = false;
-            var validationCode = bizplugin.savePlugin(plugin);
-            if(validationCode){
-                return {
-                    status: 'redirect',
-                    url: '/admin/plugins'
-                };
-            } else {
-                return {plugin: plugin};
+            if(env.req.params['submitimport'] && env.req.params['submitimport']=='Import') {
+              var url = env.req.params['pluginzipurl'];
+            }
+            else if(env.req.params['Save'] && env.req.params['submitimport']=='save') {
+              var plugin = new model.Plugin({'keyName': env.req.params['name']});
+              plugin['lastModified'] = new Date();
+              plugin['code'] = env.req.params['code'];
+              plugin['name'] = env.req.params['name'];
+              plugin['email'] = env.req.params['email'];
+              plugin['author'] = env.req.params['author'];
+              plugin['version'] = env.req.params['version'];
+              plugin['type'] = 'GDS';
+              plugin['activated'] = false;
+              var validationCode = bizplugin.savePlugin(plugin);
+              if(validationCode){
+                  return {
+                      status: 'redirect',
+                      url: '/admin/plugins'
+                  };
+              } else {
+                  return {plugin: plugin};
+              }
             }
         }
     }
